4d06e25e1a4d938230c7dba82e27402bb6d7fed4
senohirona <seno.hirona@tsunolab.net>
2020-10-11 14:25:55 +0900

    completed chapter8

---
+++ chapter-8/8-7/item2.rb
@@ -0,0 +1,11 @@
+class Drink
+    def name
+        @name
+    end
+    def name=(text)
+        @name = text
+    end
+    def size
+        @size
+    end
+end
---
+++ chapter-8/8-8/private_public2.rb
@@ -0,0 +1,14 @@
+class Foo
+    def a #public
+    end
+
+    private
+
+    def c #private
+    end
+
+    public
+
+    def d #public
+    end
+end
